4bc23d5515772e58ee294cac09d600dbd5389605
mokridevops <mokridevops@gmail.com>
2026-09-05 15:16:40 -0500

    added new conversational file

---
+++ convresation.sh
@@ -0,0 +1,9 @@
+#!/etc/bash
+
+#conversational content
+
+echo "hi I am devops engineer"
+echo "company: welcome devops engineer"
+echo "I can implement linux, shellscripting, ansible, terraform,
+      docker, kubernetes, jenkins"
+echo "company: we would like to interview you"
