4cad7c26d7ba6fb32bf64f033a558f168a71c144
mokridevops <mokridevops@gmail.com>
2026-09-06 10:28:57 -0500

    special variables

--- specialvars.sh
+++ specialvars.sh
@@ -4,4 +4,4 @@ echo "all variables passed to the script $@"
 echo "number of variables passed to the script $#"
 echo "first variable $1"
 echo "second variable $2"
-echo "name of the current script $0
+echo "name of the current script $0"
