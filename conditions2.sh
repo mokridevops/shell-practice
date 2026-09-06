8f8fb2f6bd6be49d15f0fbc27a48b19cf423152d
mokridevops <mokridevops@gmail.com>
2026-09-06 14:52:08 -0500

    conditions

---
+++ conditions2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 100 ]; then
+echo "entered number is greater than 100"
+else if [$NUMBER eq 100]; then
+echo "entered number is equal to 100"
+else
+echo "entered number is less than 100"
+fi
