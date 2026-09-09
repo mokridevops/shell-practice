01fdb716107518249bd0b2c5fc4e649fde05a037
mokridevops <mokridevops@gmail.com>
2026-09-09 12:52:52 -0500

    shell scripting

--- log_file2.sh
+++ log_file2.sh
@@ -6,7 +6,7 @@ LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log" # /var/log/shell-script/log_file2.sh.log
 
 if [ $USERID -ne 0 ]; then
-echo "run with root access"
+echo "run with root access" | tee -a $LOGS_FILE
 exit 1
 fi
 
@@ -14,10 +14,10 @@ VALIDATE()
 {
     if [ $2 -ne 0 ]; then
     echo "$2"
-    echo "installing $1 is a .... failure"
+    echo "installing $1 is a .... failure" | tee -a $LOGS_FILE
     exit 1
     else
-    echo "installing $1 is a .... success"
+    echo "installing $1 is a .... success" | tee -a $LOGS_FILE
     fi
 }
 
@@ -26,9 +26,9 @@ echo "I am continuing"
 dnf list installed mysql &>> $LOGS_FILE
 
 if [ $? -eq 0 ]; then
-    echo "mysql is already installed...Skipping"
+    echo "mysql is already installed...Skipping" | tee -a $LOGS_FILE
 else
-    echo "installing mysql"
+    echo "installing mysql" | tee -a $LOGS_FILE
     dnf install mysql -y &>> $LOGS_FILE
     VALIDATE mysql $?
     
@@ -38,9 +38,9 @@ fi
 dnf list installed nginx &>> $LOGS_FILE
 
 if [ $? -eq 0 ]; then
-    echo "nginx is already installed...Skipping"
+    echo "nginx is already installed...Skipping" | tee -a $LOGS_FILE
 else
-    echo "installing nginx"
+    echo "installing nginx" | tee -a $LOGS_FILE
     dnf install nginx -y &>> $LOGS_FILE
     VALIDATE nginx $?
 fi
