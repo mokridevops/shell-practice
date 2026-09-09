218fd9023db972ef253af5aeb48f2769647676cd
mokridevops <mokridevops@gmail.com>
2026-09-09 13:13:19 -0500

    loops1

---
+++ loops2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+# this is to get the id of the root user as an input to our script
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log" # /var/log/shell-script/log_file2.sh.log
+
+if [ $USERID -ne 0 ]; then
+echo "run with root access" | tee -a $LOGS_FILE
+exit 1
+fi
+
+VALIDATE()
+{
+    if [ $2 -ne 0 ]; then
+    echo "$2"
+    echo "installing $1 is a .... failure" | tee -a $LOGS_FILE
+    exit 1
+    else
+    echo "installing $1 is a .... success" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@
+do
+    echo "Installing $package"
+done
