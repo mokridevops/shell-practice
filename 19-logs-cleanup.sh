ecdc8e09cebbaf646df3442e99a90ec50d9f4c37
mokridevops <mokridevops@gmail.com>
2026-09-16 14:52:30 -0500

    while loop

--- 19-logs-cleanup.sh
+++ 19-logs-cleanup.sh
@@ -3,7 +3,7 @@
 SOURCE_DIR=$1
 DAYS=${2:-14} #default 14 days
 
-if [ -z SOURCE_DIR ]; then
+if [ -z $SOURCE_DIR ]; then
 
     echo "ERROR Missing parameters"
     echo "USAGE:: $0 <SOURCE-DIR> [days(optional default to 14)]"
