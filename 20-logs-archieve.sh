75141c56abae3c353f90aa7d2f08e8bd042a8ce4
mokridevops <mokridevops@gmail.com>
2026-09-17 11:44:33 -0500

    archive logs

--- 20-logs-archieve.sh
+++ 20-logs-archieve.sh
@@ -9,3 +9,15 @@ if [ -z SOURCE_DIR ] || [ -z DEST_DIR ]; then
     echo "USAGE:: $0 [source_dir] [dest_dir] [days:default 14]"
     exit 1
 fi
+
+if [ ! -d SOURCE_DIR ]; then
+    echo "Source directory : $SOURCE_DIR does not exist"
+    exit 1
+fi
+
+
+if [ ! -d DEST_DIR ]; then
+    echo "Destination directory : $DEST_DIR does not exist"
+    exit 1
+fi
+
