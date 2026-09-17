cea594bdf96bdcb1ed0c985852e745a02fda9a60
mokridevops <mokridevops@gmail.com>
2026-09-17 15:16:03 -0500

    disk utilization scripts

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -7,7 +7,7 @@ while IFS= read -r line
      do
         USAGE=$( echo $line | awk '{print $6}' | cut -d "%" -f1 )
         PARTITION=$( echo $line | awk '{print $7}' )
-        if [ "$USAGE" ge $THRESHOLD ]; then
+        if [ "$USAGE" -ge $THRESHOLD ]; then
            MESSAGE="High Disk Usage on $PARTITION: $USAGE \n"
         fi
      done <<< "$DISK_USAGE"
